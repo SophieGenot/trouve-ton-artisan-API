84aa9d38c7f446626975a8b80ec7d18095bd9e6e
SophieG <sophiegenot@hotmail.fr>
2026-02-09 18:22:27 +0100

    addbuildfront

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { Sequelize } = require('sequelize');
-require('dotenv').config({ path:  './env/.env.dev'});
+require('dotenv').config({ path: './env/.env.dev' });
 const helmet = require('helmet');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
+const path = require('path');
 
 const app = express();
 
@@ -11,8 +12,9 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(rateLimit({ windowMs: 15*60*1000, max: 100 }));
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
+// Connexion à la DB
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -25,18 +27,24 @@ const sequelize = new Sequelize(
 );
 
 const { Categorie, Specialite, Artisans } = require('./models');
-// Routes Artisan
+
+// Routes API
 const artisanRoutes = require('./routes/artisan.route');
 app.use('/api/artisans', artisanRoutes);
 
-// Route Catégorie
 const categorieRoutes = require('./routes/categorie.route');
 app.use('/api/categories', categorieRoutes);
 
-// Route Spécialité
 const specialiteRoutes = require('./routes/specialite.route');
 app.use('/api/specialites', specialiteRoutes);
 
+// SERVIR LE FRONTEND
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
