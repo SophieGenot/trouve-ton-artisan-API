0308203a2624318fd63f452b7e30a3ae0beab1c7
SophieG <sophiegenot@hotmail.fr>
2026-02-09 13:25:05 +0100

    testonlignedatabase

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
-// config/db.js
 const { Sequelize } = require('sequelize');
 require('dotenv').config({
   path: process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev'
 });
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME,     
+  process.env.DB_USER,     
+  process.env.DB_PASSWORD, 
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST, 
     dialect: 'mysql',
+    port: process.env.DB_PORT, 
     logging: false
   }
 );
